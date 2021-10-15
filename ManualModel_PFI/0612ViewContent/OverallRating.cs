8a7c73fc7d82109798f407e1a6b0a10975f80061
jaeha <haheehong1@gmail.com>
2021-10-14 21:52:06 -0400

    uploading

---
+++ ManualModel_PFI/0612ViewContent/OverallRating.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.ML.Data;
+
+namespace _0728OverallRating
+{
+    public class Features
+    {
+        
+        [LoadColumn(0)]
+        public float WindowNumber { get; set; }
+
+        [LoadColumn(1)]
+        public float WindowAreaSum { get; set; }
+        
+        /*[LoadColumn(2)]
+        public float WindowAreaAvg { get; set; }
+
+        [LoadColumn(3)]
+        public float DistanceToWindowSum { get; set; }
+
+        [LoadColumn(4)]
+        public float DistanceToWindowAvg { get; set; }
+
+        [LoadColumn(5)]
+        public float ViewVectorandWindowCenterAngleAvg { get; set; }
+
+        [LoadColumn(6)]
+        public float RoomWidth { get; set; }
+
+        [LoadColumn(7)]
+        public float RoomHeight { get; set; }
+
+        [LoadColumn(8)]
+        public float RoomDepth { get; set; }
+
+        [LoadColumn(9)]
+        public float RoomVolume { get; set; }
+
+        [LoadColumn(10)]
+        public float WholeRoomVolume { get; set; }
+
+        [LoadColumn(11)]
+        public float WindowToWallRatio { get; set; }
+
+        [LoadColumn(12)]
+        public float VWindowDispersion { get; set; }
+
+        [LoadColumn(13)]
+        public float HWindowDispersion { get; set; }
+
+        [LoadColumn(14)]
+        public float WindowLengthSum { get; set; }
+        */
+        //[LoadColumn(15)]
+        //public float WindowLengthAvg { get; set; }
+        /*
+        [LoadColumn(16)]
+        public float WindowHeightSum { get; set; }
+        */
+        //[LoadColumn(17)]
+        //public float WindowHeightAvg { get; set; }
+        /*
+        [LoadColumn(18)]
+        public float AspectRatioAvg { get; set; }
+
+        [LoadColumn(19)]
+        public float WindowHorizontalAngleSum { get; set; }
+
+        [LoadColumn(20)]
+        public float WindowHorizontalAngleAvg { get; set; }
+
+        [LoadColumn(21)]
+        public float WindowVerticalAngleSum { get; set; }
+
+        [LoadColumn(22)]
+        public float WindowVerticalAngleAvg { get; set; }
+
+        [LoadColumn(23)]
+        public float HViewAngle { get; set; }
+
+        [LoadColumn(24)]
+        public float VViewAngle { get; set; }
+
+        [LoadColumn(25)]
+        public float Z1PtsCount { get; set; }
+
+        [LoadColumn(26)]
+        public float Z2PtsCount { get; set; }
+
+        [LoadColumn(27)]
+        public float Z2PtsCountZ1PtsCount { get; set; }
+
+        [LoadColumn(28)]
+        public float Z3PtsCount { get; set; }
+
+        [LoadColumn(29)]
+        public float Z4PtsCount { get; set; }
+        */
+        [LoadColumn(30)]
+        public float Z1PtsCountRatio { get; set; }
+
+        [LoadColumn(31)]
+        public float Z2PtCountRatio { get; set; }
+
+        /*[LoadColumn(32)]
+        public float Z2PtsCountZ1PtsCountRatio { get; set; }
+        */
+        [LoadColumn(33)]
+        public float Z3PtsCountRatio { get; set; }
+
+        [LoadColumn(34)]
+        public float Z4PtsCountRatio { get; set; }
+
+        /*[LoadColumn(35)]
+        public float BuildingPtsCount { get; set; }
+
+        [LoadColumn(36)]
+        public float EquipmentPtsCount { get; set; }
+
+        [LoadColumn(37)]
+        public float TreePtsCount { get; set; }
+
+        [LoadColumn(38)]
+        public float PavementPtsCount { get; set; }
+
+        [LoadColumn(39)]
+        public float GrassPtsCount { get; set; }
+
+        [LoadColumn(40)]
+        public float WaterPtsCount { get; set; }
+
+        [LoadColumn(41)]
+        public float DynamicPtsCount { get; set; }
+
+        [LoadColumn(42)]
+        public float TreePtsCountGrassPtsCount { get; set; }
+
+        [LoadColumn(43)]
+        public float SkyPtsCount { get; set; }
+        */
+        [LoadColumn(44)]
+        public float BuildingPtsCountRatio { get; set; }
+
+        [LoadColumn(45)]
+        public float EquipmentPtsCountRatio { get; set; }
+
+        [LoadColumn(46)]
+        public float TreePtsCountRatio { get; set; }
+
+        [LoadColumn(47)]
+        public float PavementPtsCountRatio { get; set; }
+
+        [LoadColumn(48)]
+        public float GrassPtsCountRatio { get; set; }
+
+        [LoadColumn(49)]
+        public float WaterPtsCountRatio { get; set; }
+
+        [LoadColumn(50)]
+        public float DynamicPtsRatio { get; set; }
+
+        //[LoadColumn(51)]
+        //public float TreePtsCountGrassPtsCountRatio { get; set; }
+        
+        [LoadColumn(52)]
+        public float SkyPtsCountRatio { get; set; }
+        
+        
+        /*[LoadColumn(53)]
+        public float BuildingIsClosest { get; set; }
+
+        [LoadColumn(54)]
+        public float EquipmentIsClosest { get; set; }
+
+        [LoadColumn(55)]
+        public float TreeIsClosest { get; set; }
+
+        [LoadColumn(56)]
+        public float DynamicIsClosest { get; set; }
+        */
+        /*[LoadColumn(57)]
+        public float SkyIsClosest { get; set; }
+        
+        [LoadColumn(58)]
+        public float ClosestObjectDist { get; set; }
+        */
+        [LoadColumn(59)]
+        public float ElementNumber { get; set; }
+
+        [LoadColumn(60)]
+        public float FloorHeights { get; set; }
+
+        [LoadColumn(61)]
+        public float BuildingClosestDist { get; set; }
+
+        [LoadColumn(62)]
+        public float EquipmentClosestDist { get; set; }
+
+        [LoadColumn(63)]
+        public float TreeClosestDist { get; set; }
+
+        [LoadColumn(64)]
+        public float GrassClosestDist { get; set; }
+
+        [LoadColumn(65)]
+        public float WaterClosestDist { get; set; }
+
+        /*[LoadColumn(66)]
+        public float NatureClosestDist { get; set; }
+        */
+        [LoadColumn(67)]
+        public float DynamicClosestDist { get; set; }
+
+        /*[LoadColumn(68)]
+        public float SkyClosestDist { get; set; }
+        */
+        [LoadColumn(69)]
+        public float SkyCondition { get; set; }
+
+        //[LoadColumn(69)]
+        //public string ID { get; set; }
+
+        //[LoadColumn(70)]
+        //[ColumnName("Label")]
+        //public float ViewContentS { get; set; }
+
+        //[LoadColumn(71)]
+
+        //public float ViewAccessS { get; set; }
+
+        //[LoadColumn(72)]
+        //public float PrivacyS { get; set; }
+
+        [LoadColumn(73)]
+        [ColumnName("Label")]
+        public float OverallRatingS { get; set; }
+
+
+    }
+
+    public class ViewAccessPrediction
+    {
+        [ColumnName("Score")]
+        public float OverallRatingB { get; set; }
+    }
+}
---
+++ ManualModel_PFI/0612ViewContent/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.IO;
+using Microsoft.ML;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Immutable;
+using Microsoft.ML.Data;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Trainers.LightGbm;
+using Microsoft.ML.Trainers.FastTree;
+using _0728OverallRating;
+
+namespace _0728OverallRating
+{
+    class Program
+    {
+        static readonly string _trainDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "07_Train0817.CSV");
+        static readonly string _testDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "07_Test0817.CSV");
+        static readonly string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "Model.zip");
+        private static MLContext mlContext = new MLContext(seed: 1);
+        //private static ISingleFeaturePredictionTransformer<object> pfiModel;
+        //private static IDataView preprocessedTrainData;
+        private static string[] columnNames;
+        private static IDataView dataView;
+        private static IDataView testDataView;
+
+        static void Main(string[] args)
+        {
+            
+            //creat training data 
+            dataView = mlContext.Data.LoadFromTextFile<Features>(_trainDataPath, hasHeader: true, separatorChar: ',');
+            //creat test data
+            testDataView = mlContext.Data.LoadFromTextFile<Features>(_testDataPath, hasHeader: true, separatorChar: ',');
+
+            columnNames =
+                dataView.Schema
+                .Select(column => column.Name)
+                .Where(colName => colName != "Label").ToArray();
+
+            IEstimator<ITransformer> dataPrepEstimator =
+                mlContext.Transforms.Concatenate("Features", columnNames)
+                    .Append(mlContext.Transforms.NormalizeMinMax("Features"));
+
+            var model = Train(mlContext, dataView, dataPrepEstimator);
+
+            mlContext.Model.Save(model, dataView.Schema, _modelPath);
+            var mlModel2 = mlContext.Model.Load(_modelPath, out var _);
+            var predictor = ((TransformerChain<ITransformer>)mlModel2).Last();
+
+            Evaluate(mlContext, testDataView, dataPrepEstimator, model);
+            CalculatePFI(mlContext, model.Transform(testDataView), (ISingleFeaturePredictionTransformer<object>) predictor);
+            
+
+
+        }
+
+        public static ITransformer Train(MLContext mlContext, IDataView dataView, IEstimator<ITransformer> dataPrepEstimator)
+        {
+            //IDataView dataView = mlContext.Data.LoadFromTextFile<Features>(dataPath, hasHeader: true, separatorChar: ',');
+            var pipeline = mlContext.Transforms.CopyColumns(outputColumnName: "Label2", inputColumnName: "Label")
+
+                    //.Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "VendorIdEncoded", inputColumnName: "VendorId"))
+                    //.Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "RateCodeEncoded", inputColumnName: "RateCode"))
+                    //.Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "PaymentTypeEncoded", inputColumnName: "PaymentType"))
+
+                    .Append(mlContext.Transforms.Concatenate("Features",
+                "WindowNumber",
+                "WindowAreaSum",
+                "Z1PtsCountRatio",
+                "Z2PtCountRatio",
+                "Z3PtsCountRatio",
+                "Z4PtsCountRatio",
+                "BuildingPtsCountRatio",
+                "EquipmentPtsCountRatio",
+                "TreePtsCountRatio",
+                "PavementPtsCountRatio",
+                "GrassPtsCountRatio",
+                "WaterPtsCountRatio",
+                "DynamicPtsRatio",
+                "SkyPtsCountRatio",
+                "ElementNumber",
+                "FloorHeights",
+                "BuildingClosestDist",
+                "EquipmentClosestDist",
+                "TreeClosestDist",
+                "GrassClosestDist",
+                "WaterClosestDist",
+                //"NatureClosestDist",
+                "DynamicClosestDist",
+                //"SkyClosestDist",
+                "SkyCondition"
+                ))
+                    .Append(mlContext.Regression.Trainers.FastForest(new FastForestRegressionTrainer.Options() { NumberOfTrees = 4, FeatureFraction = 0.815220129448517F, LabelColumnName = @"Label2", FeatureColumnName = @"Features" }));
+
+            var model = pipeline.Fit(dataView);
+            var transformedData = model.Transform(dataView);
+
+
+            ITransformer dataPrepTransformer = dataPrepEstimator.Fit(dataView);
+
+            return model;
+
+        }
+
+        public static void CalculatePFI(MLContext mLContext, IDataView preprocessedTrainData, ISingleFeaturePredictionTransformer<object> model)
+        {
+            VBuffer<ReadOnlyMemory<char>> nameBuffer = default;
+            preprocessedTrainData.Schema["Features"].Annotations.GetValue("SlotNames", ref nameBuffer);
+            var featureColumnNames = nameBuffer.DenseValues().ToList();
+
+            ImmutableArray<RegressionMetricsStatistics> permutationFeatureImportance =
+                mlContext
+                .Regression
+                .PermutationFeatureImportance(model, preprocessedTrainData, permutationCount: 10, labelColumnName: "Label2");
+
+            var featureImportanceMetrics = 
+                permutationFeatureImportance
+                .Select((metric, index) => new { index, metric.RSquared })
+                .OrderByDescending(myFeatures => Math.Abs(myFeatures.RSquared.Mean));
+
+            Console.WriteLine("Feature\tPFI");
+            foreach(var feature in featureImportanceMetrics)
+            {
+                Console.WriteLine($"{featureColumnNames[feature.index],-20}|\t{feature.RSquared.Mean:F6}");
+
+            }
+        }
+
+        private static void Evaluate(MLContext mlContext, IDataView testDataView, IEstimator<ITransformer> trainingPipeLine, ITransformer model)
+        {
+            
+            var predictions = model.Transform(testDataView);
+
+            var metrics = mlContext.Regression.Evaluate(predictions, "Label2", "Score");
+
+            //ImmutableArray<RegressionMetricsStatistics> permutationFeatureImportance = mlContext.Regression.PermutationFeatureImportance(model, dataView, permutationCount: 3);
+
+            Console.WriteLine();
+            Console.WriteLine($"****************************************************************");
+            Console.WriteLine($"*         Model quality metrics evaluation                      ");
+            Console.WriteLine($"*---------------------------------------------------------------");
+
+            Console.WriteLine($"*         RSquared Score:             {metrics.RSquared:0.##}");
+            Console.WriteLine($"*         Root Mean Squared Error:    {metrics.RootMeanSquaredError:#.##}");
+
+
+        }
+
+
+
+    }
+}
